4f943bde4697a594703231f6a959a2d3c8e29b5c
Trevor Eyre <trevoreyre@gmail.com>
2018-10-23 08:23:13 -0600

    Bootstrapped with @vue/cli. Test publish HelloWorld component.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <HelloWorld msg="Trevor" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello {{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
