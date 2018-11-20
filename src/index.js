25bd9371c41d4c99adaba7e047daaef231cb1869
Trevor Eyre <trevoreyre@gmail.com>
2018-11-20 13:07:20 +0000

    Added AppInput component to test CSS variables. Inline CSS in build.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-export { default as HelloWorld } from './components/HelloWorld'
-export { default as Bar } from './components/Bar'
-export * from './components/Icons'
+export { default as AppInput } from "./components/AppInput";
+export { default as Bar } from "./components/Bar";
+export { default as HelloWorld } from "./components/HelloWorld";
+export * from "./components/Icons";
