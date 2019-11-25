ea434f0b41e75d622a201409c439f852866bd9a5
Trevor Eyre <trevoreyre@gmail.com>
2019-11-25 08:38:01 -0700

    Added Card component

---
+++ src/Card/Card.stories.js
@@ -0,0 +1,49 @@
+import { storiesOf } from '@storybook/vue'
+import Card from './Card.vue'
+
+storiesOf('Card', module)
+  .addParameters({ styles: { maxWidth: '500px' } })
+  .add('default', () => ({
+    components: { Card },
+    template: `
+      <Card>
+        Some Content
+      </Card>
+    `,
+  }))
+  .add('as button', () => ({
+    components: { Card },
+    template: `
+      <Card as="button">
+        Button card
+      </Card>
+    `,
+  }))
+  .add('shadows', () => ({
+    components: { Card },
+    template: `
+      <div>
+        <Card shadow="none" mb="lg">
+          No shadow
+        </Card>
+        <Card shadow="sm" mb="lg">
+          Small shadow
+        </Card>
+        <Card shadow="md" mb="lg">
+          Medium shadow
+        </Card>
+        <Card shadow="lg" mb="lg">
+          Large shadow
+        </Card>
+        <Card shadow="xl" mb="lg">
+          XL shadow
+        </Card>
+        <Card shadow="2xl" mb="lg">
+          2XL shadow
+        </Card>
+        <Card shadow="inner" mb="lg">
+          Inner shadow
+        </Card>
+      </div>
+    `,
+  }))
