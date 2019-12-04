8700b5617aeb2b29c4114420513a784b87a02ebf
Trevor Eyre <trevoreyre@gmail.com>
2019-12-03 18:17:25 -0700

    Removed @storybook/addon-cssresources. Updated dependencies.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,4 @@
 import { configure, addDecorator, addParameters } from '@storybook/vue'
-import { withCssResources } from '@storybook/addon-cssresources'
 import { withStyles } from 'storybook-addon-styles/vue'
 import AppCss from '../src/AppCss/AppCss.vue'
 import Theme from '../src/Theme/Theme.vue'
@@ -14,28 +13,6 @@ const withTheme = () => ({
   `
 })
 
-const themeFont = `
-<link href="https://fonts.googleapis.com/css?family=Lato:400,600,900" rel="stylesheet" />
-`
-
-const backgroundLight = `
-<style>
-body {
-  background: var(--color-background-light);
-}
-</style>
-`
-
-const themeToucheUnderwater = `
-<style>
-:root {
-  --color-background: #f1f3f5;
-  --color-divider: rgba(0, 33, 66, 0.24);
-}
-</style>
-`
-
-addDecorator(withCssResources)
 addDecorator(withStyles)
 addDecorator(withTheme)
 addParameters({
@@ -45,17 +22,6 @@ addParameters({
   styles: {
     padding: '40px'
   },
-  cssresources: [{
-    id: 'theme-font',
-    code: themeFont,
-    picked: true
-  }, {
-    id: 'background-light',
-    code: backgroundLight
-  }, {
-    id: 'theme-touche-underwater',
-    code: themeToucheUnderwater
-  }],
 })
 
 // Automatically import all files ending in *.stories.js
