21f8af0d044eee73f9f41b24aa686f00fcce8762
Trevor Eyre <trevoreyre@gmail.com>
2024-01-08 12:03:16 -0600

    Updated AppBar and Container stories

--- src/components/app-bar/app-bar.stories.ts
+++ src/components/app-bar/app-bar.stories.ts
@@ -12,11 +12,58 @@ const meta = {
 const Basic = {
   render: () => html`
     <slate-app-bar>
-      <slate-app-nav>
-        <div>Child 1</div>
-        <div>Child 2</div>
-        <div>Child 3</div>
-      </slate-app-nav>
+      <slate-container size="lg">
+        <slate-app-nav aria-label="Main" justify="between" wrap>
+          <slate-app-nav-content gap="md">
+            <a href="#">
+              <slate-text transform="uppercase">Home</slate-text>
+            </a>
+            <a href="#">
+              <slate-text transform="uppercase">Projects</slate-text>
+            </a>
+            <a href="#">
+              <slate-text transform="uppercase">Posts</slate-text>
+            </a>
+            <a href="#">
+              <slate-text transform="uppercase">Contact</slate-text>
+            </a>
+          </slate-app-nav-content>
+          <slate-stack class="social-links" gap="sm" direction="row">
+            <!-- <a
+              aria-label="Phone"
+              href="tel:+12086316741"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <IconPhone aria-hidden="true" />
+            </a>
+            <a
+              aria-label="Email"
+              href="mailto:trevoreyre@gmail.com"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <IconEmail aria-hidden="true" />
+            </a>
+            <a
+              aria-label="GitHub"
+              href="https://github.com/trevoreyre"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <IconLogoGitHub aria-hidden="true" />
+            </a>
+            <a
+            	aria-label="LinkedIn"
+              href="https://www.linkedin.com/in/trevoreyre"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <IconLogoLinkedIn aria-hidden="true" />
+            </a> -->
+          </slate-stack>
+        </slate-app-nav>
+      </slate-container>
     </slate-app-bar>
   `,
 }
